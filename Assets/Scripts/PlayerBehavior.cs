ba43f46458bd09c729430020e755f1a96be72b42
krillin-avi <98582420+krillin-avi@users.noreply.github.com>
2024-02-24 20:10:30 -0500

    Implement Look at Mouse Function on Player Behavior

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -15,6 +15,17 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LookAtMouse();
         
+    }
+
+    void LookAtMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector3 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
     }
 }
