720b0199d1519a78a99a22214a6d265e170142f6
Smug <zikadika2@gmail.com>
2024-03-01 15:58:32 -0500

    added pause button

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Time.timeScale = 0f;
+        }
     }
 }
